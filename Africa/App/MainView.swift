0c8b839347d043685328c0f49c71cdca49767e3c
Nic Deane <njdeane7@gmail.com>
2021-09-19 18:04:43 +1000

    Add tab bar view to MainView.swift

--- Africa/App/MainView.swift
+++ Africa/App/MainView.swift
@@ -9,7 +9,28 @@ import SwiftUI
 
 struct MainView: View {
   var body: some View {
-    Text("Main")
+    TabView {
+      ContentView()
+        .tabItem {
+          Image(systemName: "square.grid.2x2")
+          Text("Browse")
+        }
+      VideoListView()
+        .tabItem {
+          Image(systemName: "play.rectangle")
+          Text("Watch")
+        }
+      MapView()
+        .tabItem {
+          Image(systemName: "map")
+          Text("Locations")
+        }
+      GalleryView()
+        .tabItem {
+          Image(systemName: "photo")
+          Text("Gallery")
+        }
+    } //: TAB
   }
 }
 
