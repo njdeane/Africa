40d1743af87acbb04ca24efbd2d65ae56aee3b34
Nic Deane <njdeane7@gmail.com>
2021-09-19 17:27:25 +1000

    Initial Commit

---
+++ Africa/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Africa
+//
+//  Created by Nic Deane on 19/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
