c8ed05dab349fea9c30dc3a481a4b882bd0ab34b
Nic Deane <njdeane7@gmail.com>
2021-09-19 18:33:10 +1000

    Add CoverImageModel. Has struct with codable and decodable protocols.

---
+++ Africa/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by Nic Deane on 19/9/21.
+//
+
+import SwiftUI
+
+struct CoverImage: Codable, Identifiable {
+  let id: Int
+  let name: String
+}
