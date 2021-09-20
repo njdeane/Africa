5d42560f908c1bcf93206dcc9a3daba1158e1683
Nic Deane <njdeane7@gmail.com>
2021-09-20 16:54:29 +1000

    Create LocationModel with NationalParkLocation struct. Create MapAnnotationView which uses said struct to create an animating graphic. Modify MapView to have more complex map that can have the animation created embedded.

--- Africa/App/MapView.swift
+++ Africa/App/MapView.swift
@@ -6,10 +6,40 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct MapView: View {
+  
+  @State private var region: MKCoordinateRegion = {
+    var mapCoordinates = CLLocationCoordinate2D(latitude: 6.00286, longitude: 16.4377599)
+    var mapZoomLevel = MKCoordinateSpan(latitudeDelta: 70.0, longitudeDelta: 70.0)
+    var mapRegion = MKCoordinateRegion(center: mapCoordinates, span: mapZoomLevel)
+    
+    return mapRegion
+  }()
+  
+  let locations: [NationalParkLocation] = Bundle.main.decode("locations.json")
+  
   var body: some View {
-    Text("Map")
+    // 1. Basic Map
+    // Map(coordinateRegion: $region)
+    
+    // 2. Advanced Map
+    Map(coordinateRegion: $region, annotationItems: locations, annotationContent: { item in
+      // (A) PIN: Old style (always static)
+      // MapPin(coordinate: item.location, tint: .accentColor)
+      
+      // (B) Marker: New style (always static)
+      // MapMarker(coordinate: item.location, tint: .accentColor)
+      
+      // (C) Custom Basic Annotation (can be interactive)
+      MapAnnotation(coordinate: item.location) {
+        Image("logo")
+          .resizable()
+          .scaledToFit()
+          .frame(width: 32, height: 32, alignment: .center)
+      }
+    })
   }
 }
 
